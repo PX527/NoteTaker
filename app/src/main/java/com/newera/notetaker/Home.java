99d825f92cec05aba616683622a69fb7b9dc22a7
PX527 <pxlai27@gmail.com>
2019-10-12 12:30:01 +0800

    cmt

--- app/src/main/java/com/newera/notetaker/Home.java
+++ app/src/main/java/com/newera/notetaker/Home.java
@@ -2,13 +2,41 @@ package com.newera.notetaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    private Button btnAddNote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        Log.d("Home","onPause called!");
+        findViews();
+        setListener();
+    }
+
+    private void findViews(){
+        btnAddNote=findViewById(R.id.btnAddNote);
+
+    }
+
+    private void setListener(){
+        btnAddNote.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i=new Intent(Home.this,AddNoteActivity.class);
+                startActivity(i);
+            }
+
+
+        });
+
     }
 }
